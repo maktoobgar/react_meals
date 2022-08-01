101c30a1af837e9ad9ecd56a90b229ab10b1a5e6
Mahmood Heidari <mahmoodh1378@gmail.com>
2022-08-01 12:21:48 +0430

    🎉 feat: cart button bounces when items added or removed

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -4,6 +4,7 @@ CHANGELOG
 UNRELEASED
 ----------
 
+* 🎉 feat: cart button bounces when items added or removed
 * 🎉 feat: added cart module items and change amount functionality
 * 🎉 feat: add function works fine
 * 🎉 feat: Cart module shows and hides by events
--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,13 +1,26 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import cart from "../../assets/cart_shopping.png";
 import CartContext from "../../store/cart-context";
 import styles from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
 	const ctx = useContext(CartContext);
+	const [btnBump, setBtnBump] = useState(false);
+
+	const { items } = ctx;
+
+	const btnClasses = `${styles["header-button"]} ${btnBump && styles.bump}`;
+
+	useEffect(() => {
+		if (Object.keys(items).length === 0) {
+			return;
+		}
+		setBtnBump(true);
+		setTimeout(() => setBtnBump(false), 300);
+	}, [items]);
 
 	return (
-		<button className={styles["header-button"]} onClick={props.showCart}>
+		<button className={btnClasses} onClick={props.showCart}>
 			<img src={cart} alt="" />
 			<p
 				className={`${styles["header-number"]} ${
